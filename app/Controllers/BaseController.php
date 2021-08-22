fd148d8e4d9e2fae57f9dab2ca9100b1bef44534
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-08-22 15:23:12 +0300

    added getBody function

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -8,6 +8,8 @@ use CodeIgniter\HTTP\IncomingRequest;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use Psr\Log\LoggerInterface;
+use CodeIgniter\Validation\Exceptions\ValidationException;
+use Config\Services;
 
 /**
  * Class BaseController
@@ -77,5 +79,28 @@ class BaseController extends Controller
 			$input = json_decode($request->getBody(), true);
 		}
 		return $input;
+	}
+
+	public function validateRequest($input, array $rules, array $messages =[]){
+		$this->validator = Services::Validation()->setRules($rules);
+		// If you replace the $rules array with the name of the group
+		if (is_string($rules)) {
+			$validation = config('Validation');
+	
+			// If the rule wasn't found in the \Config\Validation, we
+			// should throw an exception so the developer can find it.
+			if (!isset($validation->$rules)) {
+				throw ValidationException::forRuleNotFound($rules);
+			}
+	
+			// If no error message is defined, use the error message in the Config\Validation file
+			if (!$messages) {
+				$errorName = $rules . '_errors';
+				$messages = $validation->$errorName ?? [];
+			}
+	
+			$rules = $validation->$rules;
+		}
+		return $this->validator->setRules($rules, $messages)->run($input);
 	}
 }
